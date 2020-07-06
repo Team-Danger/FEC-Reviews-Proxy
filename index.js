469df7d9b217678e6231da9880d92b86608d5e29
Janet Zheng <janet.zhengg@gmail.com>
2020-07-05 23:45:50 -0700

    changed options

--- index.js
+++ index.js
@@ -45,9 +45,9 @@ const reviewPath = path.join(__dirname, 'Reviews-Component');
 
 app.use('/api', proxy);
 app.use('/', express.static(proxyPath));
-app.use('/description', express.static(descriptionPath, staticOptions));
-app.use('/reviews', express.static(reviewPath, staticOptions));
-app.use('/reservation', express.static(reservationPath,staticOptions));
+app.use('/description', express.static(descriptionPath));
+app.use('/reviews', express.static(reviewPath));
+app.use('/reservation', express.static(reservationPath);
 
 app.listen(PORT, () => {
   console.log(`listening on proxy server: ${PORT}`)
